00828232280602e3df9b79e7fd307d77a129a8a8
MikeG27 <gorskimichal27@gmail.com>
2018-08-30 19:56:47 +0200

    done preprocessing

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ This is a temporary script file.
 #                                 Libraries
 # =============================================================================
 import os
+import shutil
 import numpy as np
 import pandas as pd
 
@@ -66,7 +67,7 @@ def read_csv(csv_list):
 def get_selected_feature(img_path_array,feature_path):
     
     """
-    Get hand data 
+    Get hand data from all features 
     
     """
     
@@ -77,6 +78,65 @@ def get_selected_feature(img_path_array,feature_path):
             data.append(i)
             
     return data
+
+def get_data_distribution(data):
+    
+
+    distr = {}
+    negative = []
+    positive = []
+    
+    for i in path_train:
+        if "negative" in i:
+            negative.append(i)
+        elif "study1_positive":
+            positive.append(i)
+    
+    distr["positive"] = positive
+    distr["negative"] = negative
+    
+    return distr
+
+
+def re_path(patch_folder):
+    
+    """
+    
+    Return array of corrected pathes of images
+    
+    """
+    
+    repath = []
+    
+    for i in patch_folder:
+        repath.append(str("/home/michal/Pulpit/MURA/"+i))
+        
+    return repath
+
+def separate_images(img_path,abnormal_class,normal_class):
+    
+    '''
+    Separate images to folder class
+    
+    '''
+    
+    n = 0 #negative index
+    p = 0 #positive index
+    
+    for i in img_path:
+        if os.path.exists(i):
+            
+            if "negative" in i:
+                n += 1
+                src = i
+                dest = (normal_class)
+                shutil.copy(src,dest+"/normal"+str(n)+".png")
+            
+            elif "positive" in i:
+                p += 1
+                src = i
+                dest = (abnormal_class)
+                shutil.copy(src,dest+"/abnormal"+str(p)+".png")
 
 
 # =============================================================================
@@ -90,29 +150,27 @@ main_structure = get_main_f(os.listdir())
 
 hand_path = os.path.join(main_dir,"hand")
 
-if os.path.exists(hand_path) == False:
-   
-    os.mkdir(hand_path)
+os.mkdir(hand_path)
 
-    # Train
-    hand_train = os.path.join(hand_path,"train")
-    os.mkdir(hand_train)
+# Train
+hand_train = os.path.join(hand_path,"train")
+os.mkdir(hand_train)
 
-    hand_train_normal = os.path.join(hand_train,"normal")
-    os.mkdir(hand_train_normal)
+hand_train_normal = os.path.join(hand_train,"normal")
+os.mkdir(hand_train_normal)
 
-    hand_train_abnormal = os.path.join(hand_train,"abnormal")
-    os.mkdir(hand_train_abnormal)
+hand_train_abnormal = os.path.join(hand_train,"abnormal")
+os.mkdir(hand_train_abnormal)
 
-    # Validation
-    hand_valid = os.path.join(hand_path,"valid")
-    os.mkdir(hand_valid)
+# Validation
+hand_valid = os.path.join(hand_path,"valid")
+os.mkdir(hand_valid)
     
-    hand_valid_normal = os.path.join(hand_valid,"normal")
-    os.mkdir(hand_valid_normal)
+hand_valid_normal = os.path.join(hand_valid,"normal")
+os.mkdir(hand_valid_normal)
     
-    hand_valid_abnormal = os.path.join(hand_valid,"abnormal")
-    os.mkdir(hand_valid_abnormal)
+hand_valid_abnormal = os.path.join(hand_valid,"abnormal")
+os.mkdir(hand_valid_abnormal)
 
 
 # =============================================================================
@@ -120,7 +178,7 @@ if os.path.exists(hand_path) == False:
 # =============================================================================
 
 #CSV
-os.chdir(main_structure[1])
+os.chdir(main_structure[2])
 csv_structure = read_csv(os.listdir())
 path_train,path_valid,label_valid,label_train = read_csv(os.listdir())
 os.chdir(main_dir)
@@ -137,20 +195,37 @@ path_valid = get_selected_feature(path_valid,"MURA-v1.1/valid/XR_HAND")
 
 
 # Get radiologist predictions  :)
-#label_train = label_train.rename(columns = {"MURA-v1.1/train/XR_SHOULDER/patient00001/study1_positive/":'img'})
-#label_valid = label_valid.rename(columns = {'MURA-v1.1/valid/XR_WRIST/patient11185/study1_positive/':'img'})
+label_train = label_train.rename(columns = {"MURA-v1.1/train/XR_SHOULDER/patient00001/study1_positive/":'img'})
+label_valid = label_valid.rename(columns = {'MURA-v1.1/valid/XR_WRIST/patient11185/study1_positive/':'img'})
+label_train = get_selected_feature(label_train,"XR_HAND")
+label_valid = get_selected_feature(label_valid,"XR_HAND")
 
-#label_train = get_selected_feature(label_train,"MURA-v1.1/train/XR_HAND")
-#label_valid = get_selected_feature(label_valid,"MURA-v1.1/valid/XR_HAND")
+
+# Get normal/abnormal distribution
+distr_hand_train = get_data_distribution(path_train)
+distr_hand_valid = get_data_distribution(path_valid)
+
+
+# Data separation
+
+# 1.Rename pathes
+path_train = re_path(path_train)
+path_valid = re_path(path_valid)
+
+# 2. Separate images to folder classes
+
+separate_images(path_train,hand_train_abnormal,hand_train_normal)
+separate_images(path_valid,hand_valid_abnormal,hand_valid_normal)
+
 
 
 # =============================================================================
-#                                   Plot Samples
+#                                    Plots
 # =============================================================================
 
 from keras.preprocessing import image
 
-plt.figure(1)
+plt.figure(1,figsize=(25,12))
 plt.suptitle("XR_HAND",fontsize=30)
 
 plt.subplot(1,3,1)
@@ -158,18 +233,25 @@ img_path = ('/home/michal/Pulpit/MURA/MURA-v1.1/train/XR_HAND/patient09900/study
 img = image.load_img(img_path)
 plt.imshow(img)
 plt.title("image1")
+plt.xlabel("Normal",fontsize=15)
 
 plt.subplot(1,3,2)
 img_path1 = ('/home/michal/Pulpit/MURA/MURA-v1.1/train/XR_HAND/patient09900/study1_positive/image2.png')
 img1 = image.load_img(img_path1)
 plt.imshow(img1)
 plt.title("image2")
+plt.xlabel("Normal",fontsize=15)
+
 
 plt.subplot(1,3,3)
 img_path2 = ('/home/michal/Pulpit/MURA/MURA-v1.1/train/XR_HAND/patient09900/study1_positive/image4.png')
 img2 = image.load_img(img_path2)
 plt.imshow(img2)
 plt.title("image3")
+plt.xlabel("Normal",fontsize=15)
+
+plt.savefig("Sample.png")
+
 
 
 
@@ -179,13 +261,32 @@ plt.title("image3")
 
 img_array = image.img_to_array(img)
 
-print("\nImage stats:\n")
-print("Image height : ",img_array.shape[0])
-print("Image width : ",img_array.shape[1])
-print("Image depth : ",img_array.shape[2])
-print("\nHand images : ")
+
+print("\n1. Hand images : ")
+
 print("\nTrain images : ",len(path_train))
 print("Valid images : ",len(path_valid))
 
+print("\n2. Hand images overview : \n")
+
+print("Train abnormal img : ",len(os.listdir(hand_train_abnormal)))
+print("Train normal img : ",len(os.listdir(hand_train_normal)))
+print("Valid abnormal img : ",len(os.listdir(hand_valid_abnormal)))
+print("Valid normal img : ",len(os.listdir(hand_valid_normal)))
+
+print("\n3. Image stats:\n")
+print("Image height : ",img_array.shape[0])
+print("Image width : ",img_array.shape[1])
+print("Image depth : ",img_array.shape[2])
+
 del img , img1 , img2 , img_path1 , img_path2 , img_path
+
+
+# =============================================================================
+#                                Model 
+# =============================================================================
+
+
+
+
 
